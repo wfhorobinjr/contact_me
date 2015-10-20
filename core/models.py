1cebcf6eac0126a0b400f197770a0fd026ed96c5
Bill <wfhorobinjr@quinnipiac.edu>
2015-10-20 23:22:27 +0000

    Add contact forms to admin panel

--- core/models.py
+++ core/models.py
@@ -6,4 +6,6 @@ class Contact(models.Model):
   message = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
+  def __unicode__(self):
+    return self.title
 # Create your models here.
