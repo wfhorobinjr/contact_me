af3b36802482d8a8c1cd92920e8924d1cd4b7d2a
Bill <wfhorobinjr@quinnipiac.edu>
2015-10-22 12:17:13 +0000

    Fix success URL

--- core/views.py
+++ core/views.py
@@ -10,7 +10,7 @@ class ContactCreateView(CreateView):
   model = Contact
   template_name = "contact/contact_form.html"
   fields = ['name', 'email', 'message']
-  sucess_url = reverse_lazy('success')
+  success_url = reverse_lazy('success')
 
 class Success(TemplateView):
   template_name = "success.html"
